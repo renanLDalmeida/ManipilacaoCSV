93b670fdb7676cd9ac2d5a5ecc108c6aeedf799c
RenanLDalmeida <rdalmeidi@gmail.com>
2020-06-29 14:24:48 -0300

    Projeto iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula27_28_29_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
